801b5c334eb7d070e6953077c6f6dbc87f82e789
AnarchyDeve <dongkyunkim0513@gmail.com>
2025-07-11 11:32:07 +0900

    테스트

---
+++ lsbs-gen5/lec1-variables.py
@@ -0,0 +1,45 @@
+# ## 1번문제
+
+# kor = 80
+# eng = 75
+# math = 55
+
+# subject = [kor,eng, math]
+# print(sum(subject)/len(subject))
+
+# ## 2번 문제
+# n = 13
+
+# if n % 2 == 1:
+#     print('{} 는 홀수입니다.'.format(n))
+# else:
+#     print('{}는 짝수입니다.'.format(n))
+
+# 3번문제
+
+# pin = "881120-1068234"
+# yyyymmdd, num = pin.split('-')
+# print(f'{yyyymmdd}\n{num}')
+
+# #4번문제
+# print( '남자' if pin[7] == '1' else '여자')
+
+# #5번문제
+# a = "a:b:c:d"
+# b = a.replace(':', '#')
+# print(b)
+
+# #6번문제
+# a = [1,3,5,4,2]
+# print(sorted(a))
+# print(sorted(a, reverse=True))
+
+# # 7번 문제
+# a = ['Life', 'is', 'too', 'short']
+# a = ' '.join(a)
+# print(a)
+
+# # 8번
+# a = (1,2,3)
+# a = a + (4,)
+# print(a)
